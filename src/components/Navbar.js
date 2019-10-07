b4430a152d927d9b518384e5e23b59ecde45ba42
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-07 11:52:11 +0200

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpons Quotes</a>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+
