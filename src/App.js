955792fbe702ad36f94f2f7686b97919cf54405b
Valentine Voyen <valentine.voyen1@gmail.com>
2019-10-07 12:02:02 +0200

    added cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import QuoteCard from './components/QuoteCard';
 
 function App() {
   return (
     <div className="App">
-
-       
+      <Navbar />
+      <QuoteCard />
     </div>
   );
 }
 
 export default App;
+
